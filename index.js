51571985051c1e18a525b7266ef6b44cf32fbaec
G1ANO <iwmuthiani@yahoo.com>
2025-06-10 12:25:20 +0300

    complete first class functions codegrade

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback) {
+  callback();
+}
+function returnsANamedFunction() {
+  function namedFunction() {
+    console.log("I'm a named function!");
+  }
+  return namedFunction;
+}
+function returnsAnAnonymousFunction() {
+  return function() {
+    console.log("I'm anonymous!");
+  };
+}
